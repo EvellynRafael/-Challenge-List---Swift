1ac1c63275f96e91ce9d408a962c00fb71e8f8e1
Evellyn Rafael <evellynsilvarafael@outlook.com.br>
2026-03-23 09:01:55 -0300

    Add average calculation for three students

---
+++ CalculatingAvarage.swift
@@ -0,0 +1,5 @@
+var student1: Double = 7
+var student2: Double = 10
+var student3: Double = 8.2
+var avarage: Double = (student1 + student2 + student3) / 3
+print("The avarage of the three students is: \(avarage)")
